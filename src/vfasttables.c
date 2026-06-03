5924cf2e4845dfdbbb36f5888782bc472dca5a4d
dedmanwalking <dedmanwalking@proton.me>
2026-06-03 13:09:02 +0000

    Add files via upload
    vfasttables >> gperf !!!
    jkjk
    but I like vfasttables because I coded it & because it's unbloated.

---
+++ src/vfasttables.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+#include "vfasttables/vfasttables.h"
+
+int main(int argc, char* argv[]){
+    vfasttable(argv, argc, true);
+
+    return 0;
+}
---
+++ src/vfasttables/parse/parse.h
@@ -0,0 +1,115 @@
+
+#include "stddef.h"
+#include "stdlib.h"
+#include "string.h"
+#include "stdbool.h"
+
+#include "help.h"
+#include "../ctx/ctx.h"
+
+// parses command-line input.
+// input:
+//  argc, argv.
+// output:
+//  vfasttables ctx that holds all relevant info:
+//      src file
+//      dst file
+//      any other options
+static vfasttables_ctx* vfasttables_parseargs(char* argv[], const int argc){
+    if(argc == 1){
+        puts("ERROR: no input files.");
+        return NULL;
+    }
+
+    if(vfasttables_check_arg(argv[1]))
+        return NULL;
+
+    vfasttables_options_e opts = VFASTTABLES_OPT_NONE;
+
+    char* const srcPath = argv[argc-1];
+    char* dstPath = NULL;
+    char* pfix = NULL;
+    char* enumname = NULL;
+    for(int i = 0; i < argc; i++){
+        if(strcmp(argv[i], "--prefixEnum") == 0) opts |= VFASTTABLES_OPT_PREFIX_ENUM;
+
+        else if(strcmp(argv[i], "-o") == 0){
+            if(argc - 2 < i){
+                puts("ERROR: no input files.");
+                return NULL;
+            }
+            else dstPath = argv[++i];
+        }
+
+        else if(strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--Prefix") == 0){
+            if(argc - 2 < i){
+                puts("ERROR: prefix specified but none present.");
+                return NULL;
+            }
+            else pfix = argv[++i];
+        }
+
+        else if(strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--Enum") == 0){
+            if(argc - 2 < i){
+                puts("ERROR: enum name specified but none present.");
+                return NULL;
+            }
+            else enumname = argv[++i];
+        }
+    }
+    if(dstPath == NULL)
+        dstPath = "out.c";
+
+    vfasttables_ctx* const restrict ctx = vfasttables_ctx_init(NULL, srcPath, dstPath, pfix, enumname, opts);
+    return ctx;
+}
+
+// expects input to be correctly formatted
+static struct vfasttables_parseinput_s vfasttables_parseinput(vfasttables_ctx* ctx){
+    char**  instr = NULL;
+    char**  intok = NULL;
+    size_t* inlen = NULL;
+    size_t  incnt = 0;
+
+    while(!feof(ctx->src)){
+        const size_t inpos = incnt++;
+        instr = realloc(instr, incnt*sizeof(*instr));
+        intok = realloc(intok, incnt*sizeof(*intok));
+        inlen = realloc(inlen, incnt*sizeof(*inlen));
+
+        int err = fscanf(ctx->src, "%lu", &inlen[inpos]);
+
+        if(err < 0){                // why does it error here? I hate anything file manipulation related in C.
+            incnt--;
+            break;
+        }
+
+        fseek(ctx->src, 1, SEEK_CUR);
+
+        instr[inpos] = malloc(inlen[inpos] + 1);
+        fread(instr[inpos], sizeof(char), inlen[inpos], ctx->src);
+        instr[inpos][inlen[inpos]] = '\0';
+        fseek(ctx->src, 1, SEEK_CUR);
+
+        size_t toklen;
+        fscanf(ctx->src, "%lu", &toklen);
+        fseek(ctx->src, 1, SEEK_CUR);
+
+        intok[inpos] = malloc(toklen+1);
+        fread(intok[inpos], sizeof(char), toklen, ctx->src);
+        intok[inpos][toklen] = '\0';
+        fseek(ctx->src, 1, SEEK_CUR);
+    }
+
+    fclose(ctx->src);
+    ctx->src = NULL;
+// like functional languages? get ready for a whole lot of this!
+// tbf it doesn't even look that ugly & it's perfectly readable. 
+// perhaps the 'default' or 'conditionless' value could be specified with a noop 'default' keyword.
+    const uint32_t mask = 
+        incnt <= 256?   255: 
+        incnt <= 65536? 65535:
+                        UINT32_MAX;
+
+    return (struct vfasttables_parseinput_s){instr, intok, inlen, incnt, mask};
+}
